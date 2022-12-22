e80c86fc5023e7e7b8710f65bddc5cd9a67da0f3
Patricio Calderon <patricio.calderon@sansano.usm.cl>
2022-12-22 19:02:51 +0100

    feat: add asset list to portfolio tests

--- test/Portfolio.t.sol
+++ test/Portfolio.t.sol
@@ -7,33 +7,53 @@ import "./mocks/MockV3Aggregator.sol";
 import "forge-std/Test.sol";
 
 contract PortfolioTest is Test {
-    uint8 public constant DECIMALS = 18;
-    int256 public constant INITIAL_ANSWER = 1 * 10**18;
-    uint256 balance = 100;
+    struct Asset {
+        uint8 decimals;
+        uint256 price;
+        uint256 balance;
+        MockV3Aggregator aggregator;
+    }
+    mapping(string => Asset) public assets;
+    string[] public symbols;
+    uint256 portfolioValue;
     Portfolio public portfolio;
 
     function setUp() public {
-        MockV3Aggregator mockV3AggregatorETH = new MockV3Aggregator(DECIMALS, INITIAL_ANSWER);
-        portfolio = new Portfolio("BTC", balance, address(mockV3AggregatorETH));
+        setAsset("BTC", 6, 20_000);
+        setAsset("ETH", 9, 2_000);
+        setAsset("XMR", 9, 200);
+        setAsset("SOL", 9, 20);
+
+        uint256 initialBalance = 100;
+        string memory initialAsset = "BTC";
+        portfolio = new Portfolio(initialAsset, initialBalance, address(assets[initialAsset].aggregator));
+        assets[initialAsset].balance = initialBalance;
+        portfolioValue = initialBalance * assets[initialAsset].price;
+        symbols.push(initialAsset);
     }
 
     // Helpers
 
+    function setAsset(string memory _symbol, uint8 _decimals, uint256 _price) public {
+        MockV3Aggregator mockV3AggregatorETH = new MockV3Aggregator(_decimals, int(_price));
+        assets[_symbol] = Asset(_decimals, _price, 0, mockV3AggregatorETH);
+    }
+
     function addAsset(string memory _symbol, uint256 _balance) public {
-        MockV3Aggregator mockV3AggregatorETH = new MockV3Aggregator(DECIMALS, INITIAL_ANSWER);
-        portfolio.addAsset(_symbol, _balance,  address(mockV3AggregatorETH));
+        portfolio.addAsset(_symbol, _balance, address(assets[_symbol].aggregator));
+        assets[_symbol].balance = _balance;
+        symbols.push(_symbol);
     }
 
     // Tests
 
     function testAddAssets() public {
-        string[4] memory assets = ["ETH", "SOL", "LINK", "XMR"];
         uint256 initialBalance = 100;
-        for (uint256 i = 0; i < assets.length; i++) {
-           addAsset(assets[i], initialBalance);
-           balance += initialBalance;
+        for (uint256 i = 1; i < symbols.length; i++) {
+           addAsset(symbols[i], (i + 1) * initialBalance);
+           portfolioValue += initialBalance * assets[symbols[i]].price;
         }
-        assertEq(portfolio.getPortfolioValue(), balance * uint(INITIAL_ANSWER));
+        assertEq(portfolio.getPortfolioValue(), portfolioValue);
     }
 
     function testChangeAssetBalance() public {
@@ -44,12 +64,12 @@ contract PortfolioTest is Test {
         uint256 addition = 200;
         portfolio.changeAssetBalance(symbol, addition, true);
         assetBalance += addition;
-        assertEq(portfolio.getAssetValue(symbol), assetBalance * uint(INITIAL_ANSWER));
+        assertEq(portfolio.getAssetValue(symbol), assetBalance * assets[symbol].price);
 
         uint256 subtraction = 50;
         portfolio.changeAssetBalance(symbol, subtraction, false);
         assetBalance -= subtraction;
-        assertEq(portfolio.getAssetValue(symbol), assetBalance * uint(INITIAL_ANSWER));
+        assertEq(portfolio.getAssetValue(symbol), assetBalance * assets[symbol].price);
 
         uint256 deletion = 10000000;
         portfolio.changeAssetBalance(symbol, deletion, false);
