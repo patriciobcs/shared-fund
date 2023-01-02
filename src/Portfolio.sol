242c42e11189ed1c429d331d350ce8049cbfe868
Patricio Calderon <patricio.calderon@sansano.usm.cl>
2023-01-02 18:39:57 +0100

    feat: add flexible proportions

--- src/Portfolio.sol
+++ src/Portfolio.sol
@@ -11,35 +11,58 @@ contract Portfolio {
     struct Asset {
         uint256 balance;
         uint256 proportion;
+        bool isFlexible;
     }
     mapping(string => Asset) public assets; // symbols => balance
     string[] public symbols; // symbols
     address public owner; // owner of the portfolio
     PriceFeedConsumer internal priceFeeds; // price feed consumer (chainlink)
+    string public baseSymbol; // symbol use as a base pair in the swaps
+    uint256 public flexibleProportion; // total amount of proportion that are static
 
-    struct Swaps {
+    struct Buys {
         string symbol;
         uint256 amount;
-        bool isBuy;
     } 
 
-    constructor(string memory _symbol, uint256 _balance, address _priceFeed) {
+    constructor(string memory _symbol, uint256 _balance, bool _isFlexible, address _priceFeed) {
         // Set portfolio's owner
         owner = msg.sender;
 
         // Add first asset to the portfolio
         priceFeeds = new PriceFeedConsumer(_symbol, _priceFeed);
         assets[_symbol].balance = _balance;
+        assets[_symbol].proportion = 100;
+        assets[_symbol].isFlexible = _isFlexible;
         symbols.push(_symbol);
+
+        if (_isFlexible) {
+            flexibleProportion = 100;
+        }
     }
 
     /**
      * @notice Adds a new asset to the portfolio
      *
      */
-    function addAsset(string memory _symbol, uint256 _balance, address _priceFeed) ownerOnly assetDoesNotExist(_symbol) public {
+    function addAsset(string memory _symbol, uint256 _balance, uint256 _proportion, bool _isFlexible, address _priceFeed) ownerOnly assetDoesNotExist(_symbol) public {
+        require(_proportion < flexibleProportion, "Not sufficient proportion available.");
+        
+        uint256 factor = 100;
+        uint256 rest = flexibleProportion - _proportion;
+
+        for (uint256 i = 0; i < symbols.length; i++) {
+            if (assets[symbols[i]].isFlexible) {
+                assets[symbols[i]].proportion = assets[symbols[i]].proportion * rest / flexibleProportion;
+            }
+        }
+
+        if (!_isFlexible) flexibleProportion = rest;
+
         priceFeeds.addPriceFeed(_symbol, _priceFeed);
         assets[_symbol].balance = _balance;
+        assets[_symbol].proportion = _proportion;
+        assets[_symbol].isFlexible = _isFlexible;
         symbols.push(_symbol);
     }
 
@@ -99,6 +122,15 @@ contract Portfolio {
         return uint256(priceFeeds.getLatestPrice(_symbol)) * assets[_symbol].balance;
     }
 
+    /**
+     * @notice Returns the asset proportion
+     *
+     * @return asset balance
+     */
+    function getAssetProportion(string memory _symbol) assetExists(_symbol) public view returns (uint256) {
+        return assets[_symbol].proportion;
+    }
+
     /** 
      * @notice Swap an asset for another
      *
@@ -106,26 +138,30 @@ contract Portfolio {
     function swapAsset(string memory _symbol, uint256 _amount, bool _isBuy) private {
           // TODO: Buy or sell the balance of the asset
           // If buy is true, then 
+          changeAssetBalance(_symbol, _amount, _isBuy);
     }
 
     /**
      * @notice Rebalance the portfolio
      *
      */
-    function rebalance() public {
+    function rebalance(uint256 buysLength) public {
         // Get the portfolio value
         uint256 portfolioValue = getPortfolioValue();
         uint256 factor = 100;
-        Swaps[] memory buys = new Swaps[](symbols.length);
-        uint64 buysLen = 0;
+        uint256 delta = 10;
+
+        Buys[] memory buys = new Buys[](buysLength);
+        uint256 buysCounter = 0; 
 
         // Calculate the proportion of each asset and rebalance
         for (uint256 i = 0; i < symbols.length; i++) {
             uint256 requiredValue =  portfolioValue * factor / assets[symbols[i]].proportion;
             uint256 currentValue = getAssetValue(symbols[i]);
             
-            // If the current value is equal to the required value, no need to rebalance the asset
-            if (currentValue == requiredValue) continue;
+            // If the current value is equal or close to the required value, no need to rebalance the asset
+            // How close the value can be to the required value is defined by the delta variable
+            if (currentValue == requiredValue || (currentValue < requiredValue + delta && currentValue < requiredValue - delta)) continue;
             
             uint256 amount = 0;
             bool isSell = true;
@@ -139,20 +175,21 @@ contract Portfolio {
             }
 
             if (isSell) {
-                swapAsset(buys[i].symbol, amount, true);
-                changeAssetBalance(symbols[i], amount, true);
+                swapAsset(buys[i].symbol, amount, false);
             } else {
-                // add the asset to the buys array
-                buys[buysLen] = Swaps(symbols[i], amount, true);
-                buysLen++;
+                // Add the asset to the buys array
+                require(buysCounter == buysLength, "The passed buys length is lower than the expected.");
+                buys[buysCounter] = Buys(symbols[i], amount);
+                buysCounter++;
             }
         }
-        
-        if (buysLen == 0) return;
 
-        for (uint256 i = 0; i < buysLen; i++) {
+        require(buysCounter != buysLength, "The passed buys length is higher than the expected.");
+        
+        if (buysCounter == 0) return;
+
+        for (uint256 i = 0; i < buysLength; i++) {
             swapAsset(buys[i].symbol, buys[i].amount, true);
-            changeAssetBalance(symbols[i], buys[i].amount, true);
         }
     }
 
--- test/Portfolio.t.sol
+++ test/Portfolio.t.sol
@@ -11,12 +11,16 @@ contract PortfolioTest is Test {
         uint8 decimals;
         uint256 price;
         uint256 balance;
+        uint256 proportion;
+        bool isFlexible;
         MockV3Aggregator aggregator;
     }
     mapping(string => Asset) public assets;
     string[] public symbols;
     uint256 portfolioValue;
     Portfolio public portfolio;
+    string initialSymbol = "BTC";
+    uint256 initialBalance = 100;
 
     function setUp() public {
         setAsset("BTC", 6, 20_000);
@@ -24,33 +28,33 @@ contract PortfolioTest is Test {
         setAsset("XMR", 9, 200);
         setAsset("SOL", 9, 20);
 
-        uint256 initialBalance = 100;
-        string memory initialAsset = "BTC";
-        portfolio = new Portfolio(initialAsset, initialBalance, address(assets[initialAsset].aggregator));
-        assets[initialAsset].balance = initialBalance;
-        portfolioValue = initialBalance * assets[initialAsset].price;
-        symbols.push(initialAsset);
+        portfolio = new Portfolio(initialSymbol, initialBalance, true, address(assets[initialSymbol].aggregator));
+        assets[initialSymbol].balance = initialBalance;
+        assets[initialSymbol].proportion = 100;
+        portfolioValue = initialBalance * assets[initialSymbol].price;
+        symbols.push(initialSymbol);
     }
 
     // Helpers
 
     function setAsset(string memory _symbol, uint8 _decimals, uint256 _price) public {
         MockV3Aggregator mockV3AggregatorETH = new MockV3Aggregator(_decimals, int(_price));
-        assets[_symbol] = Asset(_decimals, _price, 0, mockV3AggregatorETH);
+        assets[_symbol] = Asset(_decimals, _price, 0, 0, true, mockV3AggregatorETH);
     }
 
-    function addAsset(string memory _symbol, uint256 _balance) public {
-        portfolio.addAsset(_symbol, _balance, address(assets[_symbol].aggregator));
+    function addAsset(string memory _symbol, uint256 _balance, uint256 _proportion, bool _isFlexible) public {
+        portfolio.addAsset(_symbol, _balance, _proportion, _isFlexible, address(assets[_symbol].aggregator));
         assets[_symbol].balance = _balance;
+        assets[_symbol].proportion = _proportion;
+        assets[_symbol].isFlexible = _isFlexible;
         symbols.push(_symbol);
     }
 
     // Tests
 
     function testAddAssets() public {
-        uint256 initialBalance = 100;
         for (uint256 i = 1; i < symbols.length; i++) {
-           addAsset(symbols[i], (i + 1) * initialBalance);
+           addAsset(symbols[i], (i + 1) * initialBalance, 25, false);
            portfolioValue += initialBalance * assets[symbols[i]].price;
         }
         assertEq(portfolio.getPortfolioValue(), portfolioValue);
@@ -59,7 +63,11 @@ contract PortfolioTest is Test {
     function testChangeAssetBalance() public {
         string memory symbol = "ETH";
         uint256 assetBalance = 100;
-        addAsset(symbol, assetBalance);
+        addAsset(symbol, assetBalance, 25, false);
+        assets[initialSymbol].proportion = 75;
+        assertEq(portfolio.getAssetValue(symbol), assetBalance * assets[symbol].price);
+        assertEq(portfolio.getAssetProportion(symbol), 25);
+        assertEq(portfolio.getAssetProportion(initialSymbol), assets[initialSymbol].proportion);
 
         uint256 addition = 200;
         portfolio.changeAssetBalance(symbol, addition, true);
@@ -80,11 +88,93 @@ contract PortfolioTest is Test {
     function testIncorrectOwner() public {
         string memory symbol = "ETH";
         uint256 assetBalance = 100;
-        addAsset(symbol, assetBalance);
+        addAsset(symbol, assetBalance, 25, false);
 
         vm.startPrank(address(0x1));
         vm.expectRevert("Only the owner of the portfolio can call this function.");
         portfolio.changeAssetBalance("ETH", 100, true);
         vm.stopPrank();
     }
+
+    function testAddConstantProportions() public {
+        string memory ethSymbol = "ETH";
+        uint256 ethBalance = 100;
+        uint256 ethProportion = 25;
+
+        addAsset(ethSymbol, ethBalance, ethProportion, false);
+        assets[initialSymbol].proportion -= ethProportion;
+
+        assertEq(portfolio.getAssetProportion(ethSymbol), ethProportion);
+        assertEq(portfolio.getAssetProportion(initialSymbol), assets[initialSymbol].proportion);
+
+        string memory solSymbol = "SOL";
+        uint256 solBalance = 100;
+        uint256 solProportion = 50;
+
+        addAsset(solSymbol, solBalance, solProportion, false);
+        assets[initialSymbol].proportion -= solProportion;
+
+        assertEq(portfolio.getAssetProportion(ethSymbol), ethProportion);
+        assertEq(portfolio.getAssetProportion(initialSymbol), assets[initialSymbol].proportion);
+        assertEq(portfolio.getAssetProportion(solSymbol), solProportion);
+    }
+
+    function testRejectConstantProportionHigherThanAvailable() public {
+        string memory ethSymbol = "ETH";
+        uint256 ethBalance = 100;
+        uint256 ethProportion = 75;
+
+        addAsset(ethSymbol, ethBalance, ethProportion, false);
+        assets[initialSymbol].proportion -= ethProportion;
+
+        assertEq(portfolio.getAssetProportion(ethSymbol), ethProportion);
+        assertEq(portfolio.getAssetProportion(initialSymbol), assets[initialSymbol].proportion);
+
+        string memory solSymbol = "SOL";
+        uint256 solBalance = 100;
+        uint256 solProportion = 50;
+
+        vm.expectRevert("Not sufficient proportion available.");
+        addAsset(solSymbol, solBalance, solProportion, false);
+    }
+
+    function testTwoFlexibleProportionsAndOneConstantProportion() public {
+        uint256 flexibleProportion = 100;
+        string memory ethSymbol = "ETH";
+        uint256 ethBalance = 100;
+        uint256 ethProportion = 50;
+
+        addAsset(ethSymbol, ethBalance, ethProportion, true);
+        assets[initialSymbol].proportion -= ethProportion;
+
+        assertEq(portfolio.getAssetProportion(ethSymbol), ethProportion);
+        assertEq(portfolio.getAssetProportion(initialSymbol), assets[initialSymbol].proportion);
+
+        string memory solSymbol = "SOL";
+        uint256 solBalance = 100;
+        uint256 solProportion = 50;
+
+        addAsset(solSymbol, solBalance, solProportion, false);
+        flexibleProportion -= solProportion;
+        assets[ethSymbol].proportion = flexibleProportion * 100 / assets[ethSymbol].proportion;
+        assets[initialSymbol].proportion = flexibleProportion * 100 / assets[initialSymbol].proportion;
+
+        emit log_uint(solBalance);
+
+        // assertEq(portfolio.getAssetProportion(ethSymbol), assets[ethSymbol].proportion);
+        // assertEq(portfolio.getAssetProportion(initialSymbol), assets[initialSymbol].proportion);
+        // assertEq(portfolio.getAssetProportion(solSymbol), solProportion);
+    }
+
+
+    // function testRebalance() public {
+    //     uint256 initialBalance = 100;
+    //     for (uint256 i = 1; i < symbols.length; i++) {
+    //        addAsset(symbols[i], (i + 1) * initialBalance, 25, false);
+    //        portfolioValue += initialBalance * assets[symbols[i]].price;
+    //     }
+
+    //     portfolio.rebalance(0);
+    //     assertEq(portfolio.getPortfolioValue(), portfolioValue);
+    // }
 }
