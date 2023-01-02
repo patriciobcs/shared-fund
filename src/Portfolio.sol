d51626a7f700cf455bd4d355debd1e4f2daa5f90
Patricio Calderon <patricio.calderon@sansano.usm.cl>
2023-01-02 21:40:15 +0100

    feat: improve rebalance function and add complex test

--- src/Portfolio.sol
+++ src/Portfolio.sol
@@ -20,10 +20,17 @@ contract Portfolio {
     string public baseSymbol; // symbol use as a base pair in the swaps
     uint256 public flexibleProportion; // total amount of proportion that are static
 
-    struct Buys {
+    struct BuyOrder {
         string symbol;
         uint256 amount;
+        uint256 price;
     } 
+
+    event Buy(string symbol, uint256 amount, uint256 price, uint256 change);
+    event Sell(string symbol, uint256 amount, uint256 price, uint256 change);
+    event AssetState(string symbol, uint256 balance, uint256 value, uint256 required_value, uint256 proportion);
+    event AssetRemoved(string symbol);
+    event log_uint(uint256 number);
 
     constructor(string memory _symbol, uint256 _balance, bool _isFlexible, address _priceFeed) {
         // Set portfolio's owner
@@ -48,7 +55,6 @@ contract Portfolio {
     function addAsset(string memory _symbol, uint256 _balance, uint256 _proportion, bool _isFlexible, address _priceFeed) ownerOnly assetDoesNotExist(_symbol) public {
         require(_proportion < flexibleProportion, "Not sufficient proportion available.");
         
-        uint256 factor = 100;
         uint256 rest = flexibleProportion - _proportion;
 
         for (uint256 i = 0; i < symbols.length; i++) {
@@ -95,6 +101,7 @@ contract Portfolio {
                     break;
                 }
             }
+            emit AssetRemoved(_symbol);
         }
     }
 
@@ -135,10 +142,16 @@ contract Portfolio {
      * @notice Swap an asset for another
      *
      */
-    function swapAsset(string memory _symbol, uint256 _amount, bool _isBuy) private {
+    function swapAsset(string memory _symbol, uint256 _amount, bool _isBuy, uint256 _price) private {
           // TODO: Buy or sell the balance of the asset
           // If buy is true, then 
           changeAssetBalance(_symbol, _amount, _isBuy);
+
+          if (_isBuy) {
+              emit Buy(_symbol, _amount, _price, _amount * _price);
+          } else {
+              emit Sell(_symbol, _amount, _price, _amount * _price);
+          }
     }
 
     /**
@@ -151,45 +164,58 @@ contract Portfolio {
         uint256 factor = 100;
         uint256 delta = 10;
 
-        Buys[] memory buys = new Buys[](buysLength);
+        BuyOrder[] memory buys = new BuyOrder[](buysLength);
         uint256 buysCounter = 0; 
+        uint256 margin = 0;
 
         // Calculate the proportion of each asset and rebalance
         for (uint256 i = 0; i < symbols.length; i++) {
-            uint256 requiredValue =  portfolioValue * factor / assets[symbols[i]].proportion;
-            uint256 currentValue = getAssetValue(symbols[i]);
+            uint256 requiredValue =  portfolioValue * assets[symbols[i]].proportion / factor;
+            uint256 currentPrice = uint256(priceFeeds.getLatestPrice(symbols[i]));
+            uint256 currentBalance =  assets[symbols[i]].balance;
+            uint256 currentValue = currentPrice * currentBalance;
+            emit AssetState(symbols[i], currentBalance, currentValue, requiredValue, assets[symbols[i]].proportion);
             
             // If the current value is equal or close to the required value, no need to rebalance the asset
             // How close the value can be to the required value is defined by the delta variable
-            if (currentValue == requiredValue || (currentValue < requiredValue + delta && currentValue < requiredValue - delta)) continue;
+            if (currentValue == requiredValue || (currentValue < requiredValue + delta && currentValue > requiredValue - delta)) continue;
             
             uint256 amount = 0;
             bool isSell = true;
             
             // Calculate the difference (amount) between the current value and the required value
             if (currentValue > requiredValue) {
-                amount = currentValue - requiredValue;
+                uint256 diff = currentValue - requiredValue;
+                amount = diff / currentPrice;
+                margin += amount * currentPrice;
             } else if (currentValue < requiredValue) {
-                amount = requiredValue - currentValue;
+                amount = (requiredValue - currentValue) / currentPrice;
                 isSell = false;
             }
 
             if (isSell) {
-                swapAsset(buys[i].symbol, amount, false);
+                swapAsset(symbols[i], amount, false, currentPrice);
             } else {
                 // Add the asset to the buys array
-                require(buysCounter == buysLength, "The passed buys length is lower than the expected.");
-                buys[buysCounter] = Buys(symbols[i], amount);
+                require(buysCounter < buysLength, "The passed buys length is lower than the expected.");
+                buys[buysCounter] = BuyOrder(symbols[i], amount, currentPrice);
                 buysCounter++;
             }
         }
 
-        require(buysCounter != buysLength, "The passed buys length is higher than the expected.");
+        require(buysCounter == buysLength, "The passed buys length is higher than the expected.");
         
         if (buysCounter == 0) return;
 
         for (uint256 i = 0; i < buysLength; i++) {
-            swapAsset(buys[i].symbol, buys[i].amount, true);
+            uint256 diff = buys[i].amount * buys[i].price;
+            if (margin < diff) {
+                diff = margin;
+            } else {
+                margin -= diff;
+            }
+            uint256 amount = diff / buys[i].price;
+            swapAsset(buys[i].symbol, amount, true, buys[i].price);
         }
     }
 
--- test/Portfolio.t.sol
+++ test/Portfolio.t.sol
@@ -25,14 +25,13 @@ contract PortfolioTest is Test {
     function setUp() public {
         setAsset("BTC", 6, 20_000);
         setAsset("ETH", 9, 2_000);
-        setAsset("XMR", 9, 200);
-        setAsset("SOL", 9, 20);
+        setAsset("SOL", 9, 200);
+        setAsset("XMR", 9, 20);
 
         portfolio = new Portfolio(initialSymbol, initialBalance, true, address(assets[initialSymbol].aggregator));
         assets[initialSymbol].balance = initialBalance;
         assets[initialSymbol].proportion = 100;
         portfolioValue = initialBalance * assets[initialSymbol].price;
-        symbols.push(initialSymbol);
     }
 
     // Helpers
@@ -40,6 +39,7 @@ contract PortfolioTest is Test {
     function setAsset(string memory _symbol, uint8 _decimals, uint256 _price) public {
         MockV3Aggregator mockV3AggregatorETH = new MockV3Aggregator(_decimals, int(_price));
         assets[_symbol] = Asset(_decimals, _price, 0, 0, true, mockV3AggregatorETH);
+        symbols.push(_symbol);
     }
 
     function addAsset(string memory _symbol, uint256 _balance, uint256 _proportion, bool _isFlexible) public {
@@ -47,7 +47,6 @@ contract PortfolioTest is Test {
         assets[_symbol].balance = _balance;
         assets[_symbol].proportion = _proportion;
         assets[_symbol].isFlexible = _isFlexible;
-        symbols.push(_symbol);
     }
 
     // Tests
@@ -55,7 +54,7 @@ contract PortfolioTest is Test {
     function testAddAssets() public {
         for (uint256 i = 1; i < symbols.length; i++) {
            addAsset(symbols[i], (i + 1) * initialBalance, 25, false);
-           portfolioValue += initialBalance * assets[symbols[i]].price;
+           portfolioValue += (i + 1) * initialBalance * assets[symbols[i]].price;
         }
         assertEq(portfolio.getPortfolioValue(), portfolioValue);
     }
@@ -164,15 +163,35 @@ contract PortfolioTest is Test {
         assertEq(portfolio.getAssetProportion(solSymbol), solProportion);
     }
 
+    function testRebalance() public {
+        // BTC = 100, ETH = 10, SOL = 10, XMR = 10
+        uint256 balance = 10;
+        // BTC = 25, ETH = 25, SOL = 25, XMR = 25
+        uint256 proportion = 25;
+        // Initial State - [ BTC = 100% ], PV = 20_000 * 100 = 2_000_000
+        addAsset("ETH", 1_000, 25, false);
+        addAsset("SOL", 10_000, 25, false);
+        addAsset("XMR", 10_000, 25, false);
+        portfolioValue = 6_200_000; 
+        // New State - [ BTC = 25%, ETH = 25%, SOL = 25%, XMR = 25% ]
+        // Before Rebalance
+        // | Sym | Balance * Price = Value  | Proportion                        | 
+        // | BTC | 20_000 * 100 = 2_000_000 | 2_000_000 / 6_200_000 = 0.32 = 32 |
+        // | ETH | 2_000 * 1000 = 2_000_000 | 2_000_000 / 6_200_000 = 0.32 = 32 |
+        // | SOL | 200 * 10000 = 2_000_000  | 2_000_000 / 6_200_000 = 0.32 = 32 |
+        // | XMR | 20 * 10000 = 200_000     | 200_000   / 6_200_000 = 0.03 = 3  |
+        // | PV  | 2_000_000 + 2_000_000 + 2_000_000 + 200_000 = 6_200_000      |
+        assertEq(portfolio.getPortfolioValue(), portfolioValue);
+        
+        portfolio.rebalance(1);
+        // After Rebalance
+        // | Sym | Previous Value | New Value | Swap   | Change      | New Proportion | Operation |
+        // | BTC | 2_000_000      | 1_550_000 | -22    | -440_000    | 0.25 = 25      | Sell      |
+        // | ETH | 2_000_000      | 1_550_000 | -225   | -450_000    | 0.25 = 25      | Sell      |
+        // | SOL | 2_000_000      | 1_550_000 | -2250  | -450_000    | 0.25 = 25      | Sell      |
+        // | XMR | 200_000        | 1_550_000 | 67500  | 1_350_000   | 0.25 = 25      | Buy       |
+        // | PV  | 
 
-    // function testRebalance() public {
-    //     uint256 initialBalance = 100;
-    //     for (uint256 i = 1; i < symbols.length; i++) {
-    //        addAsset(symbols[i], (i + 1) * initialBalance, 25, false);
-    //        portfolioValue += initialBalance * assets[symbols[i]].price;
-    //     }
-
-    //     portfolio.rebalance(0);
-    //     assertEq(portfolio.getPortfolioValue(), portfolioValue);
-    // }
+        assertEq(portfolio.getPortfolioValue(), portfolioValue);
+    }
 }
