0bb1e71ea4230b55cceefd71b011849dac0054d6
Patricio Calderon <patricio.calderon@sansano.usm.cl>
2023-01-25 11:34:19 +0100

    feat: add wagmi connect

--- app/src/Header/Header.tsx
+++ app/src/Header/Header.tsx
@@ -2,6 +2,45 @@ import React from 'react';
 import './Header.scss';
 import whiteLogo from "./../assets/logo-white.png";
 import {Link} from "react-router-dom";
+import Profile from '../Profile/Profile';
+ 
+import { WagmiConfig, createClient, configureChains, mainnet } from 'wagmi'
+ 
+import { alchemyProvider } from 'wagmi/providers/alchemy'
+import { publicProvider } from 'wagmi/providers/public'
+ 
+import { InjectedConnector } from 'wagmi/connectors/injected'
+import { MetaMaskConnector } from 'wagmi/connectors/metaMask'
+import { WalletConnectConnector } from 'wagmi/connectors/walletConnect'
+ 
+// Configure chains & providers with the Alchemy provider.
+// Two popular providers are Alchemy (alchemy.com) and Infura (infura.io)
+const { chains, provider, webSocketProvider } = configureChains(
+  [mainnet],
+  [alchemyProvider({ apiKey: process.env.REACT_APP_GOERLI_API_KEY }), publicProvider()],
+)
+
+const client = createClient({
+  autoConnect: true,
+  connectors: [
+    new MetaMaskConnector({ chains }),
+    new WalletConnectConnector({
+      chains,
+      options: {
+        qrcode: true,
+      },
+    }),
+    new InjectedConnector({
+      chains,
+      options: {
+        name: 'Injected',
+        shimDisconnect: true,
+      },
+    }),
+  ],
+  provider,
+  webSocketProvider,
+})
 
 function Header() {
     return (
@@ -13,6 +52,9 @@ function Header() {
                 <a href="#">About Us</a>
                 <a href="#">Documentation</a>
                 <button className="header__create-fund-button"><Link to="/fund">Your fund</Link> </button>
+                <WagmiConfig client={client}>
+                    <Profile />
+                </WagmiConfig>
             </nav>
         </header>
     );
